066df3ee5fe2bce0fab7109256950d4eac42ae8b
belod <sxbravo@gmail.com>
2024-12-04 10:15:13 +0100

    Kliens szkriptekhez további értelmező kommentek

--- frontend_alt.js
+++ frontend_alt.js
@@ -6,30 +6,35 @@ A Promise objektum állapotai:
 2. fullfilled: kapcsolat létrejött
 3. rejected: kapcsolat visszautasítva
 */
-
+//Átveszi a műveletet a Html-ből és eléri az input mezőkben megadott értékeket.
 function calculate(operation) {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
-
+//Megvizsgáljuk, hogy kapott-e értéket a két input mező.
     if(isNaN(num1) || isNaN(num2)) {
         document.getElementById('result').textContent = "Kérem adjon meg egy számot!";
         return;
     }
+/*REST API végpont elérése és az adatok aszinkron 
+elküldése a végpontnak JSON formátumban.*/
     fetch('http://localhost:3000/api/math/calculate',{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ num1, num2, operation })
     })
-
+//Első .then: ha nem jött adat az API-tól, hibaüzenetet ad vissza
     .then(response => {
         if (!response.ok) {
             throw new Error('Hiba történt a kérés feldolgozása során!');
         }
+//Egyébként visszaadja az API-tól érkezett adatokat a response objektumban.
         return response.json();
     })
+//Második .then: az API-tól érkezett eredmény megjelenítése a Html-ben.
     .then(data => {
         document.getElementById('result').textContent = data.result;
     })
+//Hibaüzenet megjelenítése a Html-ben az eredmény helyén.
     .catch(error => {
         document.getElementById('result').textContent = `Hiba: ${error.message}`;
     });
