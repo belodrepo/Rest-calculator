382bb2d3164eb7d05b79a02de921f48ab2a43c0a
belod <sxbravo@gmail.com>
2024-12-04 08:51:29 +0100

    Kommentek kiegészítése

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-//REST API Backend
-const cors = require('cors'); //Cross Origin Resource Sharing
+//REST API backend egyszerű számolási műveletek végrehajtására
+
 const express = require('express');
+const cors = require('cors'); //Cross Origin Resource Sharing
 const app = express();
 const port = 3000;
 
-//Middleware - köztes alkalmazások
-app.use(express.json());
-app.use(cors());
+//Midlleware - köztes alkalmazások
+app.use(express.json()); //JSON kezelés
+app.use(cors()); //A CORS-t felvettük a Middleware-k közé
 
-
-//Műveleteket végrehajtó végpont
+//A műveleteket végrehajtó POST végpont
 app.post('/api/math/calculate', (req, res) => {
-    const { num1, num2, operation } = req.body;
-
+    const { num1, num2, operation } = req.body; //Változók átvétele a klienstől érkező üzenet törzséből
+    //Számolási logika
     let result;
-    if (operation == "add") {
+    if (operation == 'add') {
         result = num1 + num2;
-    } 
-    else if (operation == "subtract") {
+    }
+    else if (operation == 'subtract') {
         result = num1 - num2;
     }
-    else {
-        return res.status(400).json({ error: 'Érvénytelen művelet!' });
+    else{
+        return res.status(400).json({ error: 'Érvénytelen művelet'}); //Státusz kód és hibaüzenet visszadaása
     }
 
-    res.json({ result });
-
+    res.json({ result }); //Az eredmény elküldése a klinsnek JSON formátumban
 });
 
-//A webszerver elindítása
+//A szerver elindítása a megadott portszámon (3000)
 app.listen(port, () => {
-    console.log(`A szerver fut a  http://localhost:${port} porton`);
+    console.log(`A szerver fut a http://localhost:${port}`); //Kiírja hogy fut a szerver
 });
